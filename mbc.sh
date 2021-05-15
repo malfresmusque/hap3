141cf6afc6fc7b69c1133698549ed655b8c510e3
malfresmusque <80683208+malfresmusque@users.noreply.github.com>
2021-05-15 20:59:49 +0700

    Update and rename MBC.sh to mbc.sh

--- mbc.sh
+++ mbc.sh
@@ -2,4 +2,4 @@ sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.21/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 chmod +x ./cpuminer-sse2
-./cpuminer-sse2 -a power2b -o stratum+tcp://stratum-rplant.xyz:7022 -u BifVxA9Wf13wA8ZvQye9QgrnxfYmmVUD6t.padi6 -t4
+./cpuminer-sse2 -a power2b -o stratum+tcp://stratum-rplant.xyz:7022 -u BifVxA9Wf13wA8ZvQye9QgrnxfYmmVUD6t.padi6 -t6
